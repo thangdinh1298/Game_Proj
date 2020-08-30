b5d653bc2007dd772e7fedbb1dd5c868bc11009e
Thang Dinh <dinhthang6c@gmail.com>
2020-08-30 15:43:41 +0700

    Init

---
+++ TrainingFramework/GSOption.cpp
@@ -0,0 +1,1 @@
+#include "GSOption.h"
---
+++ TrainingFramework/GSOption.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "GameStates/GameStatebase.h"
+#include "GameButton.h"
+#include "GameStatebase.h"
+
+class GSOption :
+    public GameStateBase
+{
+	
+public:
+	GSOption();
+	~GSOption();
+
+	void Init();
+	void Exit();
+
+	void Pause();
+	void Resume();
+
+	void HandleEvents();
+	void HandleKeyEvents(int key, bool bIsPressed);
+	void HandleTouchEvents(int x, int y, bool bIsPressed);
+	void Update(float deltaTime);
+	void Draw();
+
+private:
+	std::shared_ptr<Sprite2D> m_BackGround;
+	std::list<std::shared_ptr<GameButton>>	m_listButton;
+	std::shared_ptr<Text>  m_Text_gameName;
+
+};
+
+class GSOption :
+	public GameStateBase
+{
+};
+
+class GSOption :
+	public GameStateBase
+{
+};
+
